d1750353bda1f4da3abba458f5717fa3073ced06
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-12-05 17:15:32 +0100

    feat: :sparkles: Add Box's prototype and its functions, creation of Singleton with IIFE

--- src/box/box.js
+++ src/box/box.js
@@ -1,1 +1,51 @@
-º
+
+/**
+ * Prototipo Box
+ */
+
+// Import singlenton of MrMeeseeks
+let singlentonMrMeeseeks = require('../mrMeeseeks/mrmeeseeks');
+
+// * Creation of Box's Prototype
+function Box() {
+  this.name = "Rick's Box";
+  // In function Constructor set mrMeeseeks's object 
+  //to null because the creation of mrMeeseeks will be done by a method
+  this.mrMeeseeks = null;
+}
+
+// * Creaction of "methods" of Prototype Box
+
+// ? create a new object MrMeeseeks from the MrMeeseeks's prototype 
+Box.prototype.createMrMeeseeks = function() {
+  return "MrMeeseeks";
+}
+
+Box.prototype.pressButton = function(reality) {
+  // Parameter "reality" is the collection of MrMeeseks
+
+  return "Press";
+}
+
+Box.prototype.getProtoMrMeeseeks = function() {
+  return "proto";
+}
+
+// Creation of Box's Singleton
+
+// Using of IIFE to execute createSingletonBox()
+var factorySingleton = (function createSingletonBox() {
+  const singletonBox = new Box();
+
+  function getBox() {
+    return singletonBox;
+  }
+
+  // Return OpenApi of Box's module to get and respect POLE rules
+  return {
+    getBox
+  }
+})();
+
+// Export the OpenApi object in a new property "singletonBox" of "exports" object
+exports.singletonBox = factorySingleton;
